0d5c576ea58fded7f1a51501323401f346609d8d
All-Light <alexander1868@hotmail.com>
2026-05-24 16:04:20 +0200

    remove pattern duplicates

--- next.config.ts
+++ next.config.ts
@@ -13,18 +13,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "drive.google.com",
       },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com"
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com"
-      },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com"
-      },
     ],
   },
 };
