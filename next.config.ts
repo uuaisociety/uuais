1ee0dd2dfc28957e64901dd87ebc191ae02f53f3
All-Light <alexander1868@hotmail.com>
2026-08-08 20:50:31 +0200

    Fix npm audit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    instantInsights: {
+      validationLevel: "manual-warning",
+    },
+  },
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
